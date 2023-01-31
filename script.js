45f5dd49ffdbd8794545e510c76a16769044af86
Jason Hensley <jasonhensley1989@gmail.com>
2023-01-31 12:34:38 -0600

    clock bug not resetting and staying at zero

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ let resetBtn = document.getElementById("reset");
 let clock = document.getElementById("clock");
 let startClockBtn = document.getElementById("startClock");
 let resetClock = document.getElementById("resetClock");
-let textSign = document.getElementById("text");
 
 
 let clickCount = 0;
-let clockCount = 0;
+
+
+
+const resetCount = () => {
+    clickCount = 0;
+    number.innerHTML = 0;
+    console.log("Reset Button Clicked")
+}
 
 const currentCount = () =>  {   
     clickCount += 1;
@@ -16,44 +22,39 @@ const currentCount = () =>  {
     console.log("Hi, youve clicked the counter")
 }
 
-const resetCount = () => {
-    clickCount = 0;
-    number.innerHTML = 0;
-    console.log("Reset Button Clicked")
-}
+// clock functions
+
+
 
 const startClockCount = () => {   
-    for(let i = clockCount; i < clockCount + 1; i++) {
-        let display = setInterval(changeClock, 1000)
+    for(let i = 0; i < 60; i++) {
+        let clockCount = 0;
+        display = setInterval(changeClock, 1000)
+        clock.innerHTML = clockCount;
         console.log(clockCount)
     }
-    setTimeout(changeText, 1000);
+  
+    
 }
-const changeClock = () => {
-    clockCount++;
-    clock.innerHTML = clockCount;
-}
-
 
 const resetClockCount = () => {
-    clockCount = -1;
-    textSign.innerHTML = ""
-    // changeText();
+    clockCount = 0;
+    if(clockCount < 0) {
+        let newClockCount = 0;
+        clock.innerHTML = newClockCount;
+    }
+}
+const changeClock = () => {
+    clockCount++;    
 }
 
-const changeText = () => {
-    let textBox = "Dang";
-    for(let i = 0; i < textBox.length; i++) {
-        textSign.innerHTML +=  textBox[i];
-        console.log(textSign)
-    }
-    // textSign.innerHTML = "Da"
-}
+
+
+
 
 
 number.addEventListener("click", currentCount);
 resetBtn.addEventListener("click", resetCount);
 startClockBtn.addEventListener("click", startClockCount);
 resetClock.addEventListener("click", resetClockCount);
-textSign.addEventListener("click", changeText)
 
